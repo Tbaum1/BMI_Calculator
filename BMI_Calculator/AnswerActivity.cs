654f3f248f7769b9f2d9a1ab47be1cfe2da0a99a
ANTHONY TENNENBAUM <anthony.tennenbaum@yahoo.com>
2018-09-09 10:33:50 -0500

    Initial

---
+++ BMI_Calculator/AnswerActivity.cs
@@ -0,0 +1,31 @@
+﻿using System;
+using Android.App;
+using Android.Content;
+using Android.OS;
+using Android.Widget;
+
+namespace BMI_Calculator
+{
+    [Activity(Label = "AnswerActivity")]
+    public class AnswerActivity : Android.App.Activity
+    {
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+
+            // Create your application here
+            SetContentView(Resource.Layout.answer_activity);
+            string answer = Intent.GetStringExtra("answer") ?? "Not Recv";
+
+            var txtViewAnswer = FindViewById<TextView>(Resource.Id.txtViewAnswer);
+            var goBack = FindViewById<Button>(Resource.Id.btnOK);
+
+            
+            txtViewAnswer.Text = answer;
+
+            goBack.Click += delegate{
+                this.Finish();
+            };
+        }
+    }
+}
---
+++ BMI_Calculator/MainActivity.cs
@@ -0,0 +1,64 @@
+﻿using Android.App;
+using Android.OS;
+using Android.Support.V7.App;
+using Android.Runtime;
+using Android.Widget;
+using Android.Content;
+using System;
+using Environment = System.Environment;
+
+namespace BMI_Calculator
+{
+    [Activity(Label = "@string/app_name", Theme = "@style/AppTheme", MainLauncher = true)]
+    public class MainActivity : AppCompatActivity
+    {
+        public Type answer_activity { get; set; }
+
+        protected override void OnCreate(Bundle savedInstanceState)
+        {
+            base.OnCreate(savedInstanceState);
+            // Set our view from the "main" layout resource
+            SetContentView(Resource.Layout.activity_main);
+
+            var editTxtHeight = FindViewById < EditText >( Resource.Id.editTxtHeight);
+            var editTxtWeight = FindViewById < EditText>( Resource.Id.editTxtWeight);
+            var btnCalc = FindViewById<Button>(Resource.Id.btnCalc);
+            double answer;
+            Intent answerActivity = new Intent(this, typeof(AnswerActivity));
+            btnCalc.Click += delegate
+            {
+                double height = Math.Pow(Double.Parse(editTxtHeight.Text), 2);
+                int weight = int.Parse(editTxtWeight.Text);
+                double temp = (weight / height) * 703;
+                answer =  Math.Round(temp, 2);           
+                checkAnswer();
+            };
+
+            void checkAnswer()
+            {
+                if (answer < 18.50)
+                {
+                    answerActivity.PutExtra("answer", "Underwieght" + Environment.NewLine + Convert.ToString(answer) + "% BMI");
+                    StartActivity(answerActivity);
+                }
+                else if (answer >= 18.54 && answer <= 24.99)
+                {
+                    answerActivity.PutExtra("answer", "Normal or Heathly Weight" + Environment.NewLine + Convert.ToString(answer) + "% BMI");
+                    StartActivity(answerActivity);
+                }
+                else if (answer >= 25.00 && answer <= 29.99)
+                {
+                    answerActivity.PutExtra("answer", "Overweight" + Environment.NewLine + Convert.ToString(answer) + "% BMI");
+                    StartActivity(answerActivity);
+                }
+                else if (answer >= 30.00)
+                {
+                    answerActivity.PutExtra("answer", "Obese" + Environment.NewLine + Convert.ToString(answer) + "% BMI");
+                    StartActivity(answerActivity);
+                }
+            }
+        }
+
+
+    }
+}
